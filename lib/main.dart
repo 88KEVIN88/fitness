c93f408d2c5959fea636997062c99d5eeb147362
Kevin <azzolari.kevin.studente@itispaleocapa.it>
2024-05-21 08:30:36 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       title: 'Allenamento Tracker',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
           selectedItemColor: Colors.blue,
         ),
       ),
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
